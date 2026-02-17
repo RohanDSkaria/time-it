c4c2bc0ff047ae66f1ea1c4344b0b35de6feacae
RohanDSkaria <rohandanny484@gmail.com>
2026-02-17 10:15:38 +0530

    add initial CLI implementation

---
+++ internal/cli/cli.go
@@ -0,0 +1,34 @@
+package cli
+
+import (
+	"log"
+
+	"github.com/RohanDSkaria/time-it/internal/db"
+	"github.com/RohanDSkaria/time-it/internal/repository"
+	"github.com/RohanDSkaria/time-it/internal/service"
+)
+
+func Run(args []string) {
+	if len(args) == 1 {
+		return
+	}
+
+	db, err := db.Open()
+	if err != nil {
+		log.Fatalf("DB connection failed: %v", err)
+	}
+
+	repo := repository.New(db)
+	svc := service.New(repo)
+	cmd := args[1]
+
+	switch cmd {
+
+	case "stop":
+		svc.Stop()
+
+	default:
+		svc.Start(cmd)
+	}
+
+}
