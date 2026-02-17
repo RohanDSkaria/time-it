59f24a083e5d622c57349e0f4268ef5dce6265c7
RohanDSkaria <rohandanny484@gmail.com>
2026-02-17 07:47:17 +0530

    add SQLite initialization and migration runner

---
+++ internal/db/db.go
@@ -0,0 +1,61 @@
+package db
+
+import (
+	"database/sql"
+	"errors"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/sqlite"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+
+	_ "modernc.org/sqlite"
+)
+
+func Open() (*sql.DB, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	path := filepath.Join(home, "time-it.db")
+
+	db, err := sql.Open("sqlite", path)	
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	if err := runMigrations(db); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func runMigrations(db *sql.DB) error {
+	driver, err := sqlite.WithInstance(db, &sqlite.Config{})
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"sqlite",
+		driver,
+	)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return err
+	}
+
+	return nil
+}
